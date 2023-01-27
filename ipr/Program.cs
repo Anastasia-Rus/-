ecdc651e951c160320982548e3967960521cdd4b
Anastasia-Rus <121459807+Anastasia-Rus@users.noreply.github.com>
2023-01-27 11:28:07 +0300

    Добавили первый метод

--- ipr/Program.cs
+++ ipr/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+﻿string[] Method1(string[] input, int x) 
+{
+    string[] output = new string[Method2(input,x)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= x) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
